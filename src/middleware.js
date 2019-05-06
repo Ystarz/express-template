226a48f6980a193f53c5d8f084ba7a74782c3085
huang puguang <puguang_huang@fpi-inc.com>
2019-05-06 15:09:57 +0800

    feat: checkArgs中间件注释及实例

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,11 @@
+/**
+ * 参数检查中间件
+ * 所有需要读取请求参数的路由都应该调用主动该中间件
+ * 该中间件会根据提供的参数名称去寻找指定的参数
+ * 若都找到则将其放在 req.Datas 中，若有一个没找到则直接返回 "必填字段空缺"
+ * 
+ * @param {Array} argsName 接口需要的参数名列表
+ */
 export function checkArgs(argsName=[]) {
     return (req, res, next) => {
         // 判断请求类型 获取不同的参数存放对象
--- src/router/HelloWorld.js
+++ src/router/HelloWorld.js
@@ -2,13 +2,19 @@ import express from 'express'
 let router = express.Router()
 import { checkArgs } from '../middleware'
 
+// 响应 get 请求并返回
 router.get('/hello', (req, res) => {
     res.send('world')
 })
-
+// 响应 post 请求并解析 body 中的参数
 router.post('/hello', (req, res) => {
-    let arg = req.reqDatas
+    let arg = req.body
     res.send(arg)
+})
+// 使用了 checkArgs 中间件来进行参数检查，通过检查后需要的参数会被放在 req.reqDatas 中
+router.get('/needArgs', checkArgs(['a', 'b', 'c']), (req, res) => {
+    let args = req.reqDatas
+    res.send(args)
 })
 
 export default router
