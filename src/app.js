3ba2c0811b86bd52d2618504590bcba78caf0203
huang puguang <puguang_huang@fpi-inc.com>
2019-05-06 10:53:08 +0800

    feat: 引入跨域及日志中间件

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import express from 'express'
 let app = express()
+
 // 引入post解析中间件
 let bodyParser = require('body-parser')
 let multer = require('multer')
 let upload = multer()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(upload.array())
+
+// 引入跨域中间件
+let cors = require('cors')
+let corsOptions = {
+    origin: '*'
+}
+app.use(cors(corsOptions))
+
+// 引入日志中间件
+let morgan = require('morgan')
+app.use(morgan('short'))
 
 // 引入路由
 import { HelloWorld } from './router'
