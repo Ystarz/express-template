267f29ceec9af5cad76025e26bc9a892c3cbf86a
huang puguang <puguang_huang@fpi-inc.com>
2019-05-06 10:39:21 +0800

    feat: 添加post解析中间件

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from 'express'
-
 let app = express()
+// 引入post解析中间件
+let bodyParser = require('body-parser')
+let multer = require('multer')
+let upload = multer()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(upload.array())
 
 app.get('/hello', (req, res) => {
     res.send('world')
